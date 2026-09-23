a735186a12f97d728a0c4c3a4424e8e9454cf365
lv.jj <lv@gmail.com>
2026-09-23 12:03:13 +0200

    exdavittestdemo

--- test_demo.py
+++ test_demo.py
@@ -1,3 +1,6 @@
+
+import pytest
+
 def somme (a,b):
     resultat = a+b
     return resultat
@@ -14,4 +17,17 @@ def test_somme():
 def test_my_min():
     assert my_min(8,5)==3
     assert my_min(9,8)==1
-                
+
+def my_div(a,b):
+    return a/b
+
+def test_my_div():
+    assert my_div(8,4)== 2
+
+def test_my_div_error():
+    with pytest.raises(ZeroDivisionError,match="division by zero"):
+        my_div(8,0)
+
+    
+
+
